27b4c5adab426bfa30b3826e893138958680b2c2
Vuong Ho <hochivuong2002@gmail.com>
2025-02-11 21:09:43 -0500

    [feat] Add static sectioning strategy

--- tests/unit/test_models/test_sectioning_strategy.py
+++ tests/unit/test_models/test_sectioning_strategy.py
@@ -1,6 +1,6 @@
 import json
 import unittest
-# from core.models.sectioning_strategy import StaticSectioningStrategy
+from core.models.sectioning_strategy import StaticSectioningStrategy
 from core.models.note import Note
 
 class TestSectioningStrategyModel(unittest.TestCase):
@@ -19,7 +19,6 @@ class TestSectioningStrategyModel(unittest.TestCase):
         """Runs before each test."""
 
         self.note = Note(self.sample_study_note_plaintext)
-        pass  
 
     def test_static_sectioning_by_num_of_section(self):
         """ Test creating a node from plaintext"""
@@ -29,8 +28,9 @@ class TestSectioningStrategyModel(unittest.TestCase):
         static_section_strat = StaticSectioningStrategy()
 
         actual_section_list = static_section_strat.process_by_num_of_section(processed_note, num_of_section)
+        expected_section_list = self.expected_static_sectioning_by_num_of_section
 
-        assert (actual_section_list == self.expected_static_sectioning_by_num_of_section)
+        self.assertEqual(actual_section_list, expected_section_list)
 
     def tearDown(self):
         """Runs after each test."""
