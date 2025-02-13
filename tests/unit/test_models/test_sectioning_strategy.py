cca7ac46be89b18d814a7805dd386cc676883677
Vuong Ho <hochivuong2002@gmail.com>
2025-02-12 21:31:16 -0500

    [tests] Fix test_sectioning_strategy with section

--- tests/unit/test_models/test_sectioning_strategy.py
+++ tests/unit/test_models/test_sectioning_strategy.py
@@ -2,6 +2,7 @@ import json
 import unittest
 from core.models.sectioning_strategy import StaticSectioningStrategy
 from core.models.note import Note
+from core.models.section import Section
 
 class TestSectioningStrategyModel(unittest.TestCase):
     
@@ -13,7 +14,10 @@ class TestSectioningStrategyModel(unittest.TestCase):
             cls.sample_study_note_plaintext = f.read()
         
         with open("tests/data/sample_static_sectioning_by_num_of_section.json", "r", encoding="utf-8") as f:
-            cls.expected_static_sectioning_by_num_of_section = json.load(f)
+            expected_section_list = []
+            for section_text in json.load(f):
+                expected_section_list.append(Section(section_text))
+            cls.expected_static_sectioning_by_num_of_section = expected_section_list
 
     def setUp(self):
         """Runs before each test."""
@@ -30,7 +34,10 @@ class TestSectioningStrategyModel(unittest.TestCase):
         actual_section_list = static_section_strat.process_by_num_of_section(processed_note, num_of_section)
         expected_section_list = self.expected_static_sectioning_by_num_of_section
 
-        self.assertEqual(actual_section_list, expected_section_list)
+        self.assertEqual(len(actual_section_list), len(expected_section_list))
+
+        for i in range(len(expected_section_list)):
+            self.assertEqual(actual_section_list[i].content, expected_section_list[i].content)
 
     def tearDown(self):
         """Runs after each test."""
