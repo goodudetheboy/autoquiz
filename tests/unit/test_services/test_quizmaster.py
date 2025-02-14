80a595ce81f77b5a4ae29026d6d30598bf27dc27
Vuong Ho <hochivuong2002@gmail.com>
2025-02-13 22:01:15 -0500

    [test] Update test_quizmaster

--- tests/unit/test_services/test_quizmaster.py
+++ tests/unit/test_services/test_quizmaster.py
@@ -23,20 +23,40 @@ class TestQuizmasterServices(unittest.TestCase):
             Test creating a set of quizzes from a Note object and static
             sectioning strategy with num of section
         """
+        model = "gpt-4o"
         processed_note = self.note
         num_of_section = 10
         quiz_per_section = 5
         static_section_strat = StaticSectioningStrategy(num_of_section)
         
-        quizmaster = Quizmaster()
+        quizmaster = Quizmaster(model)
 
-        quiz_list = quizmaster.create_quiz(
+        quiz_list = quizmaster.create_quiz_list(
             note=processed_note,
             section_strategy=static_section_strat,
             quiz_per_section=quiz_per_section
         )
 
         self.assertEqual(len(quiz_list), num_of_section * quiz_per_section)
+
+        for quiz in quiz_list:
+            self.assertIn("question", quiz)
+            self.assertIn("choices", quiz)
+            quiz_choices = quiz["choices"]
+            self.assertEqual(len(quiz_choices) == 4)
+
+            for i in range(len(quiz_choices)):
+                choice = quiz_choices[i]
+                self.assertIn("description", choice)
+                self.assertIn("isCorrect", choice)
+
+                # Enforce first choice is always correct 
+                if i == 0:
+                    self.assertTrue(quiz_choices[i])
+                else:
+                    self.assertFalse(quiz_choices[i])
+
+            self.assertTrue(quiz_choices)
 
     def tearDown(self):
         """Runs after each test."""
