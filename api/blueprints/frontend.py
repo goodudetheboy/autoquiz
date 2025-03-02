f9a95220880d3e096e9aec85080a663c233d93d3
Vuong Ho <hochivuong2002@gmail.com>
2025-03-02 00:30:30 -0500

    [refactor] Split up the main.css

--- api/blueprints/frontend.py
+++ api/blueprints/frontend.py
@@ -6,3 +6,8 @@ frontend_bp = Blueprint("frontend", __name__)
 @frontend_bp.route('/')
 def index():
     return render_template("pages/main.html", title="autoQuiz: You want it, we quiz it")
+
+@frontend_bp.route('/history')
+def history():
+    return render_template("pages/history.html")
+
