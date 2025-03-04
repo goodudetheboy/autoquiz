6d06af3c1310b4c5c7422e77f99ce59ac48b5c2c
Vuong Ho <hochivuong2002@gmail.com>
2025-03-04 13:08:43 -0500

    [build] Add deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -d "./venv" ]; then
+  echo "Environment has not been initialized yet, doing it now"
+  ./scripts/init.sh
+fi
+
+if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+  # Deploy on Windows
+  source venv/Scripts/activate
+  waitress-serve --listen=127.0.0.1:5000 app:app
+else
+  # Deploy on Linux
+  source venv/bin/activate
+  gunicorn -w 4 -b 127.0.0.1:5000 wsgi:app
+fi
+
+export FLASK_ENV=development
+export TESTING=true
